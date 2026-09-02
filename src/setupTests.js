e247484d649c70daffa42c4dd37bcab845dd0aa0
toibawani <toibawani14@gmail.com>
2026-09-02 20:51:53 +0530

    Update test setup configuration

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,39 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock HTMLCanvasElement.prototype.getContext for JSDOM
+if (typeof window !== 'undefined' && window.HTMLCanvasElement) {
+  window.HTMLCanvasElement.prototype.getContext = () => ({
+    fillRect: () => {},
+    clearRect: () => {},
+    getImageData: (x, y, w, h) => ({ data: new Array(w * h * 4) }),
+    putImageData: () => {},
+    createImageData: () => [],
+    setTransform: () => {},
+    scale: () => {},
+    drawImage: () => {},
+    save: () => {},
+    fillText: () => {},
+    restore: () => {},
+    beginPath: () => {},
+    moveTo: () => {},
+    lineTo: () => {},
+    closePath: () => {},
+    stroke: () => {},
+    translate: () => {},
+    rotate: () => {},
+    arc: () => {},
+    fill: () => {},
+    ellipse: () => {},
+    measureText: () => ({ width: 0 }),
+    transform: () => {},
+    resetTransform: () => {},
+    createRadialGradient: () => ({
+      addColorStop: () => {},
+    }),
+    createLinearGradient: () => ({
+      addColorStop: () => {},
+    }),
+  });
+}
